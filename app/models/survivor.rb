4835a9232e2f53d879e61a37f2f32bc7e7720088
anachronicsofa <alineramosv48@gmail.com>
2020-06-16 14:08:33 -0300

    Validates updated

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,2 +1,9 @@
 class Survivor < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 42 }
+  validates :birthdate, presence: true, format: { without: /\A[a-zA-Z]+\z/,
+                                                  message: 'only allows numbers' }
+  validates :gender, presence: true, length: { maximum: 10 }
+  validates :latitude, presence: true, length: { maximum: 3 }
+  validates :longitude, presence: true, length: { maximum: 3 }
+  validates :reports_received, numericality: { only_integer: true }, allow_blank: true
 end
