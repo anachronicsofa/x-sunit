36ad48a59a52447d6ac78e0d657a9841d2b3134d
anachronicsofa <alineramosv48@gmail.com>
2020-06-16 14:09:02 -0300

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :survivors
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resources :survivors
+    end
+  end
 end
