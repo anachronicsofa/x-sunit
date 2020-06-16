e3034309eff0a7988d9b3ac171dd34d248df20b8
anachronicsofa <alineramosv48@gmail.com>
2020-06-16 20:08:54 -0300

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :survivors
+      resources :survivors do
+        collection do
+          get :show_alphabetic_order
+          get :percentage_of_abducted
+          get :percentage_of_non_abducted
+        end
+      end
     end
   end
 end
