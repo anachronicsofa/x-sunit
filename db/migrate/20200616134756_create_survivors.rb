8d4ba508a69616672a88a458da64cc8f2946543f
anachronicsofa <alineramosv48@gmail.com>
2020-06-16 11:59:22 -0300

    Migrate validates

--- db/migrate/20200616134756_create_survivors.rb
+++ db/migrate/20200616134756_create_survivors.rb
@@ -6,8 +6,8 @@ class CreateSurvivors < ActiveRecord::Migration[6.0]
       t.string :gender
       t.float :latitude
       t.float :longitude
-      t.integer :reports_received
-      t.boolean :abducted
+      t.integer :reports_received, default: 0
+      t.boolean :abducted, default: false
 
       t.timestamps
     end
