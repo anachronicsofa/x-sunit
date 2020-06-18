bf62b55c4692ab5138efef78ae44fba90067de33
anachronicsofa <alineramosv48@gmail.com>
2020-06-17 21:58:06 -0300

    improving migration

--- db/migrate/20200616134756_create_survivors.rb
+++ db/migrate/20200616134756_create_survivors.rb
@@ -1,11 +1,11 @@
 class CreateSurvivors < ActiveRecord::Migration[6.0]
   def change
     create_table :survivors do |t|
-      t.string :name
-      t.date :birthdate
-      t.string :gender
-      t.float :latitude
-      t.float :longitude
+      t.string :name, null: false
+      t.date :birthdate, null: false
+      t.string :gender, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
       t.integer :reports_received, default: 0
       t.boolean :abducted, default: false
 
